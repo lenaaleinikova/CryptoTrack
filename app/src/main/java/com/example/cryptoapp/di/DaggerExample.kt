4374013539fc2e1478bd5548f4fa67e3f82c53f1
lena_aleinikova <alenka_aleinikova@mail.ru>
2024-10-07 22:19:23 +0300

    create example DaggerComponent

---
+++ app/src/main/java/com/example/cryptoapp/di/DaggerExample.kt
@@ -0,0 +1,21 @@
+package com.example.cryptoapp.di
+
+import dagger.Component
+import javax.inject.Inject
+
+class Car @Inject constructor(private var engine: Engine)
+
+class Engine @Inject constructor( private var fuel: Fuel)
+
+class Fuel @Inject constructor(){
+    private val fuel = "disel"
+}
+
+@Component
+interface DaggerComponent {
+    fun getCar(): Car
+    fun getEngine(): Engine
+    fun getFuel(): Fuel
+}
+
+//private var car: Car =DaggerDaggerComponent.create().getCar()
